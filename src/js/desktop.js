72c535c41f5d99cd3cf8bd3b30da5d3577e36188
user 0629 <user0629@adsn.work>
2021-08-02 20:16:29 +0300

    first

---
+++ src/js/desktop.js
@@ -0,0 +1,59 @@
+//import "core-js/es6/promise";
+//import ImagePreloader from "image-preloader";
+
+// const preloader = new ImagePreloader(function(info) {
+//   console.log("image with source %s is loaded with status %s", info.value.src, info.status);
+// });
+
+let isStarted = false;
+
+function replaceClasses(doc) {
+  // Find height class and replace it width inline style...
+  // Makes hight of block equal to height of head
+  let e = doc.querySelectorAll(".h-head-eq");
+  for (let i = 0; i < e.length; i++) {
+    e[i].style.height = e[i].style.minHeight = desktop_height + "px";
+  }
+}
+
+function handleResize() {
+  document.getElementById("b-left").style.width = "calc((100% - " + desktop_width + "px) / 2)";
+  document.getElementById("b-right").style.width = "calc((100% - " + desktop_width + "px) / 2)";
+  document.body.style.paddingTop = desktop_header + "px";
+
+  document.getElementById("b-top").style.maxWidth = desktop_width + "px";
+  document.getElementById("b-top").style.height = desktop_height + "px";
+
+  replaceClasses(document);
+
+  EQCSS.apply();
+}
+
+window.init = function() {
+  if (isStarted) {
+    handleResize();
+    return;
+  }
+
+  isStarted = true;
+
+  // Do some preparation, data loading, etc ...
+
+  // Initial style settings ..
+  handleResize();
+
+  // Start ...
+  showBranding();
+};
+
+function showBranding() {
+  // Set click action
+  document.body.onclick = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    //window.open(URL + encodeURIComponent(window.feedData[window.currentItemId].url + UTM));
+    window.open(URL);
+  };
+
+  document.getElementsByTagName("html")[0].classList.add("ready");
+}
---
+++ src/js/mobile.js
@@ -0,0 +1,6 @@
+//import "core-js/es6/promise";
+//import ImagePreloader from "image-preloader";
+
+window.init = function() {
+  console.log("Mobile started");
+};
